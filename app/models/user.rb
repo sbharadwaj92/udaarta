792e0116eb95a1becfdeb045c4e88541bfd0b583
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2015-05-27 21:40:49 +0530

    fixed the merge bug

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :name, presence: true
-  
-  devise :database_authenticatable, :registerable, :confirmable, :timeoutable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
